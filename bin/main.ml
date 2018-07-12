07c8afe9da73be262e99b30004a4ff1f2603e2ca
Calascibetta Romain <romain.calascibetta@gmail.com>
2018-07-12 16:25:50 +0200

    Receive the reply from binary

--- bin/main.ml
+++ bin/main.ml
@@ -46,9 +46,11 @@ let load_file filename =
   Bytes.unsafe_to_string rs
 
 let cstruct : Cstruct.t Lambda.Type.t = Lambda.Type.abstract "Cstruct.t"
+let error : unit Lambda.Type.t = Lambda.Type.abstract "Block.error"
 
 let eval ?(primitives = []) s =
-  match Lambda.Request.parse ~primitives ~gamma:[ "Cstruct.t", (Lambda.Type.abstract_injection cstruct) ] s with
+  match Lambda.Request.parse ~primitives ~gamma:[ "Cstruct.t", (Lambda.Type.abstract_injection cstruct)
+                                                ; "Block.error", (Lambda.Type.abstract_injection error) ] s with
   | Error _ as e -> e
   | Ok (ast, typ) ->
     let Lambda.Type.V typ = Lambda.Type.typ typ in
@@ -57,12 +59,17 @@ let eval ?(primitives = []) s =
     | Error e -> Fmt.kstrf (fun e -> Error (`Msg e)) "%a" Lambda.pp_error e
 
 let request ~block_n ~block_size ~block_output ?(primitives = []) s =
-  match Lambda.Request.parse ~primitives ~gamma:[ "Cstruct.t", (Lambda.Type.abstract_injection cstruct) ] s with
+  match Lambda.Request.parse ~primitives ~gamma:[ "Cstruct.t", (Lambda.Type.abstract_injection cstruct)
+                                                ; "Block.error", (Lambda.Type.abstract_injection error) ] s with
   | Error _ as e -> e
   | Ok v ->
     let (ast, typ) = v in
     Fmt.(pf stdout) "Ready to send: %a:%a.\n%!" Lambda.Parsetree.pp ast Lambda.Parsetree.Type.pp typ;
-    let encoder = Lambda_protobuf.Rpc.Encoder.default (Lambda_protobuf.make (ast, typ, Int64.of_int block_output)) (Int64.of_int block_size) (Int64.of_int block_n) in
+    let encoder = Lambda_protobuf.Rpc.Encoder.default
+        Lambda_protobuf.Rpc.Encoder.Request
+        (Lambda_protobuf.make (ast, typ, Int64.of_int block_output))
+        (Int64.of_int block_size)
+        (Int64.of_int block_n) in
     Ok encoder
 
 let make_socket addr port =
@@ -72,11 +79,12 @@ let make_socket addr port =
   Unix.connect socket Unix.(ADDR_INET (inet_addr, port));
   Unix.out_channel_of_descr socket, Unix.in_channel_of_descr socket
 
+let src = Cstruct.create 0x8000
+let dst = Cstruct.create 0x8000
+let tmp = Bytes.create 0x8000
+
 let send_request oc blocks encoder =
   let open Lambda_protobuf in
-
-  let src = Cstruct.create 0x8000 in
-  let dst = Cstruct.create 0x8000 in
 
   let rec go encoder =
     Format.printf "Evaluation of: %a.\n%!" Rpc.Encoder.pp encoder;
@@ -86,7 +94,7 @@ let send_request oc blocks encoder =
       Fmt.(pf stdout) "> Await.\n%!";
 
       let block_n, block_consumed = Rpc.Encoder.block t in
-      let len = min (Cstruct.len dst) (String.length blocks.(Int64.to_int block_n) - block_consumed) in
+      let len = min (Cstruct.len src) (String.length blocks.(Int64.to_int block_n) - block_consumed) in
       Cstruct.blit_from_string blocks.(Int64.to_int block_n) block_consumed src 0 len;
 
       go (Rpc.Encoder.refill 0 len t)
@@ -112,24 +120,70 @@ let send_request oc blocks encoder =
       flush oc in
   go encoder
 
-let repl ?(block_n = 0) ?(block_size = 0) ~block_output socket blocks =
+let receive_reply ic decoder =
+  let open Lambda_protobuf in
+
+  let clean_and_return buffer =
+    let ret = Buffer.contents buffer in
+    Buffer.clear buffer; ret in
+
+  let buffer_protobuf = Buffer.create 0x800 in
+  let buffer_block = Buffer.create 0x800 in
+
+  let rec go blocks t = match Rpc.Decoder.eval src t with
+    | `Await t ->
+      let len = input ic tmp 0 (Bytes.length tmp) in
+      Cstruct.blit_from_bytes tmp 0 src 0 len;
+      go blocks (Rpc.Decoder.refill 0 len t)
+    | `Flush (t, `Protobuf, raw) ->
+      Buffer.add_string buffer_protobuf (Cstruct.to_string raw);
+      go blocks (Rpc.Decoder.flush t)
+    | `Flush (t, `Block n, raw) ->
+      let blocks =
+        if Int64.to_int n = List.length blocks
+        then blocks
+        else (clean_and_return buffer_block :: blocks) in
+      Buffer.add_string buffer_block (Cstruct.to_string raw);
+      go blocks (Rpc.Decoder.flush t)
+    | `Error (_, err) ->
+      Fmt.epr "Retrieve an error: %a.\n%!" Rpc.Decoder.pp_error err
+    | `End _ ->
+      let blocks =
+        if Buffer.length buffer_block > 0
+        then (clean_and_return buffer_block :: blocks)
+        else blocks in
+
+      let reply = Buffer.contents buffer_protobuf in
+      let decoder = Pbrt.Decoder.of_bytes (Bytes.unsafe_of_string reply) in
+      let reply = Pb.decode_value decoder in
+      let Lambda.Parsetree.V { v; pp; _ } = value_from reply in
+
+      Fmt.(pf stdout) ">>> receive: %a.\n%!" pp v;
+      List.iteri (fun idx block -> Fmt.(pf stdout) "block %d:\n\n%a\n%!" idx pp_string block) (List.rev blocks) in
+  go [] decoder
+
+let primitives =
+  Lambda.[ L.primitive "Block.read" Type.[ int64; list cstruct; ] Type.(lwt (result unit error)) (fun _ _ -> assert false) ]
+
+let repl ?(block_n = 0) ?(block_size = 0) ~block_output (socketo, socketi) blocks =
   let rec go () =
     output_string stdout "# ";
     flush stdout;
 
     match input_line stdin with
     | expr ->
-      (match request ~block_n ~block_size ~block_output expr with
+      (match request ~block_n ~block_size ~block_output ~primitives expr with
        | Ok encoder ->
-         send_request socket blocks encoder;
+         send_request socketo blocks encoder;
          Fmt.(pf stdout) "Send a lambda-calculus expression.\n%!";
+         receive_reply socketi (Lambda_protobuf.Rpc.(Decoder.default Reply));
          go ()
        | Error (`Msg e) -> Fmt.epr "Retrieve an error: %s" e)
     | exception End_of_file -> () in go ()
 
 let main host port output blocks =
   let blocks = List.map load_file blocks in
-  let socket, _ = make_socket host port in
+  let socket = make_socket host port in
 
   let status =
     let lens = List.map String.length blocks in

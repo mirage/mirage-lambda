ca33d7624832b2a0ffa7ffe25db6b03e627afe4c
Thomas Gazagnaire <thomas@gazagnaire.org>
2018-07-13 15:26:42 +0200

    Minor formatting change

--- mirage/unikernel.ml
+++ mirage/unikernel.ml
@@ -70,23 +70,40 @@ module Main (B: BLOCK) (S: TCP) = struct
          | Lambda.Parsetree.Prm primitive -> Primitives.add k primitive primitives
          | _ -> Fmt.invalid_arg "Invalid expression as primitive")
       Primitives.empty
-      Lambda.[ primitive   "Block.pp_error"           [ formatter; error; ]                     Type.unit                            B.pp_error
-             ; primitive   "Block.pp_write_error"     [ formatter; write_error; ]               Type.unit                            B.pp_write_error
-             ; L.primitive "Block.disconnect"         [ Type.unit ]                             Type.(lwt unit)                      (fun () -> B.disconnect b)
-             ; L.primitive "Block.get_info"           []                                        Type.(lwt info)                      B.(get_info b)
-             ; primitive   "read_write"               [ info ]                                  Type.bool                            (fun b -> b.Mirage_block.read_write)
-             ; primitive   "sector_size"              [ info ]                                  Type.int                             (fun b -> b.Mirage_block.sector_size)
-             ; primitive   "size_sectors"             [ info ]                                  Type.int64                           (fun b -> b.Mirage_block.size_sectors)
-             ; L.primitive "Block.read"               Type.[ int64; list cstruct; ]             Type.(lwt (result unit error))       B.(fun off cs -> read b off cs >|= fun res -> match res with
-                 | Ok () -> res
-                 | Error err ->
-                 Fmt.epr "Got an error: %a.\n%!" B.pp_error err; res)
-             ; L.primitive "Block.write"              Type.[ int64; list cstruct; ]             Type.(lwt (result unit write_error)) B.(write b)
-             ; primitive   "Cstruct.to_string"        [ cstruct ]                               Type.string                          Cstruct.to_string
-             ; primitive   "Cstruct.of_string"        [ Type.string ]                           cstruct                              (fun s -> Cstruct.of_string s)
-             ; primitive   "Cstruct.blit"             Type.[ cstruct; int; cstruct; int; int; ] Type.unit                            Cstruct.blit
-             ; primitive   "Cstruct.blit_to_string"   Type.[ cstruct; int; bytes; int; int; ]   Type.unit                            Cstruct.blit_to_bytes
-             ; primitive   "Cstruct.blit_from_string" Type.[ string; int; cstruct; int; int; ]  Type.unit                            Cstruct.blit_from_string ],
+      Lambda.[
+        primitive   "Block.pp_error"           [ formatter; error; ]
+          Type.unit                            B.pp_error
+      ; primitive   "Block.pp_write_error"     [ formatter; write_error; ]
+          Type.unit                            B.pp_write_error
+      ; L.primitive "Block.disconnect"         [ Type.unit ]
+          Type.(lwt unit)                      (fun () -> B.disconnect b)
+      ; L.primitive "Block.get_info"           [ Type.unit ]
+          Type.(lwt info)                      (fun () -> B.get_info b)
+      ; primitive   "read_write"               [ info ]
+          Type.bool                            (fun b -> b.Mirage_block.read_write)
+      ; primitive   "sector_size"              [ info ]
+          Type.int                             (fun b -> b.Mirage_block.sector_size)
+      ; primitive   "size_sectors"             [ info ]
+          Type.int64                           (fun b -> b.Mirage_block.size_sectors)
+      ; L.primitive "Block.read"               Type.[ int64; list cstruct; ]
+          Type.(lwt (result unit error))       B.(fun off cs ->
+              read b off cs >|= fun res -> match res with
+              | Ok ()     -> res
+              | Error err ->
+                Fmt.epr "Got an error: %a.\n%!" B.pp_error err; res)
+      ; L.primitive "Block.write"              Type.[ int64; list cstruct; ]
+          Type.(lwt (result unit write_error)) B.(write b)
+      ; primitive   "Cstruct.to_string"        [ cstruct ]
+          Type.string                          Cstruct.to_string
+      ; primitive   "Cstruct.of_string"        [ Type.string ]
+          cstruct                              (fun s -> Cstruct.of_string s)
+      ; primitive   "Cstruct.blit"             Type.[ cstruct; int; cstruct; int; int; ]
+          Type.unit                            Cstruct.blit
+      ; primitive   "Cstruct.blit_to_string"   Type.[ cstruct; int; bytes; int; int; ]
+          Type.unit                            Cstruct.blit_to_bytes
+      ; primitive   "Cstruct.blit_from_string" Type.[ string; int; cstruct; int; int; ]
+          Type.unit                            Cstruct.blit_from_string
+      ],
     List.fold_left
       (fun gamma (k, v) -> Gamma.add k v gamma)
       Gamma.empty
